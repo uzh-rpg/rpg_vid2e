2e42f6ebdaeb75bc5d2c1f19992ffbcb222530bb
magehrig <mathias.gehrig@gmail.com>
2020-12-02 11:32:49 +0100

    rm -j

--- esim_py/setup.py
+++ esim_py/setup.py
@@ -39,7 +39,6 @@ class CMakeBuild(build_ext):
         build_args = ['--config', cfg]
 
         cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-        build_args += ['--', '-j']
 
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
