531943e07c1062095d890ccaa3ba0d63503bc0f5
Daniel Gehrig <daniel.gehrig18@gmail.com>
2021-05-11 18:24:26 +0200

    implemented different ct for positive and negative events and refractory period

--- esim_torch/test.py
+++ esim_torch/test.py
@@ -8,7 +8,9 @@ from esim_torch import EventSimulator_torch
 
 
 if __name__ == "__main__":
-    esim_torch = EventSimulator_torch(contrast_threshold=0.2)
+    esim_torch = EventSimulator_torch(contrast_threshold_neg=0.2,
+                                      contrast_threshold_pos=0.2,
+                                      refractory_period=0)
 
     print("Loading images")
     image_files = sorted(glob.glob("../esim_py/tests/data/images/images/*.png"))
